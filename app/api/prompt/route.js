b344fe0b6c76a30861c110916d26006b60880d95
woody <gelius24@hotmail.com>
2024-04-24 00:02:34 -0400

    sysrhtszrh

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,11 +1,12 @@
 import { connectToDB } from "@/utils/database"
 import Prompt from "@/models/prompt"
 
-export const GET = async () => {
+export const GET = async (req) => {
+  const test = req;
   try {
     await connectToDB()
     const prompts = await Prompt.find({}).populate('creator')
-    return new Response(JSON.stringify(prompts), { status: 200 })
+    return req(JSON.stringify(prompts), { status: 200 })
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 })
   }
